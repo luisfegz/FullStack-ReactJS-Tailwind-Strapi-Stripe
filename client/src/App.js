9a03ba072321fd8075e124909305efcb5cf9e7cc
luisfegz <87459956+luisfegz@users.noreply.github.com>
2023-08-29 15:23:15 -0500

    M 9:30

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
